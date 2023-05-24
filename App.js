6e2b3a27c8f37a6957b54ee5c21d7db69d751fb9
Rae62 <ludovic.duhamel-62260@outlook.fr>
2023-05-24 11:51:34 +0200

    googleauthworking

--- App.js
+++ App.js
@@ -1,13 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'expo-dev-client';
+import { GoogleSignin , GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import React, { useState, useEffect } from 'react';
+
+
+
 
 export default function App() {
-  return (
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+
+      // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    const user_sign_in = auth().signInWithCredential(googleCredential);
+    user_sign_in.then((user) => {
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  if (initializing) return null;
+
+
+  GoogleSignin.configure({
+    webClientId: '637525771906-atmh7bk219e3di17jhc3vi6tld6jsdkh.apps.googleusercontent.com',
+  });
+  
+
+  if(!user) {
+    return(
+      <View style={styles.container}>
+        <GoogleSigninButton
+        style={{height: 300 , width: 65 , marginTop:300}}
+        onPress={onGoogleButtonPress}
+        />
+      </View>
+    )
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Welcome , {user.displayName}</Text>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
